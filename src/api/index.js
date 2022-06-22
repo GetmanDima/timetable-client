8d2b80cd4f48f8c59619a422ad9bba934858eb4d
GetmanDima <dima.getman12112001@gmail.com>
2022-06-23 01:35:50 +0300

    Адрес api теперь зависит от production/dev

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,22 @@
-import {API_URL} from "@env";
+import {API_URL, NODE_ENV} from "@env";
 import axios from "axios";
+import Constants from "expo-constants";
+const {manifest} = Constants;
+
+const devApiUrl = `http://${manifest.debuggerHost.split(":").shift()}:5000/api`;
+const apiUrl = NODE_ENV === "production" ? API_URL : devApiUrl;
+
+console.log(apiUrl);
 
 export const getHost = () => {
   return axios.create({
-    baseURL: API_URL,
+    baseURL: apiUrl,
   });
 };
-console.log(API_URL);
+
 export const getAuthHost = accessToken => {
   const $authHost = axios.create({
-    baseURL: API_URL,
+    baseURL: apiUrl,
   });
 
   const authInterceptor = config => {
